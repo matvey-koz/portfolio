7d272940721ff9c67ff517e5fa0c38974c161b65
matvey-koz <m.key.201015@gmail.com>
2024-05-02 19:42:30 -0400

    e

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
+let slide2 = document.querySelector("body > div.slide-2");
+slide2.style.display = "none";
+slide2.style.scale = "0";
+
 function slide() {
-	let slide2 = document.querySelector("body > div.slide-2");
 	slide2.style.display = "block";
 	slide2.style.scale = "1";
 }
